a72bd8db2eeb15ebc483bc4a19dbee8bf54d740a
Zhang613rgb <37904393+Zhang613rgb@users.noreply.github.com>
2024-09-04 14:10:05 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 ip_version_priority = "ipv6"
 
 source_urls = [
-    "https://raw.githubusercontent.com/fanmingming/live/main/tv/m3u/ipv6.m3u",
+    # "https://raw.githubusercontent.com/fanmingming/live/main/tv/m3u/ipv6.m3u",
     # "https://live.fanmingming.com/tv/m3u/ipv6.m3u",
     "酒店组播",
     "https://raw.githubusercontent.com/Supprise0901/TVBox_live/main/live.txt",
@@ -33,12 +33,12 @@ source_urls = [
 url_blacklist = [
     "epg.pw/stream/",
     "103.40.13.71:12390",
-    "[2409:8087:1a01:df::4077]/PLTV/",
+    # "[2409:8087:1a01:df::4077]/PLTV/",
     "8.210.140.75:68",
     "154.12.50.54",
     "yinhe.live_hls.zte.com",
     "8.137.59.151",
-    "[2409:8087:7000:20:1000::22]:6060",
+    # "[2409:8087:7000:20:1000::22]:6060",
     "histar.zapi.us.kg",
     "www.tfiplaytv.vip",
     "dp.sxtv.top",
